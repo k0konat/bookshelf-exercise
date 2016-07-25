f639f52ea6ef15ed62e4b01e6f8be127ab029aee
k0konat <konate1kk@gmail.com>
2016-07-25 02:45:16 -0500

    Modified Swager config

--- src/main/java/com/intuit/craft/apidocs/SwaggerConfig.java
+++ src/main/java/com/intuit/craft/apidocs/SwaggerConfig.java
@@ -8,6 +8,7 @@ import com.google.common.base.Predicates;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -31,7 +32,19 @@ public class SwaggerConfig {
           .select()                                  
           .apis(RequestHandlerSelectors.any())   
           .paths(Predicates.not(PathSelectors.regex("/error")))                         
-          .build();  
+          .build().apiInfo(apiInfo());  
+    }
+    
+    private ApiInfo apiInfo() {
+        ApiInfo apiInfo = new ApiInfo(
+                "Bookshelf Service",
+                "This is a rest service built by using Spring-Boot.",
+                "API TOS",
+                "konate1kk@gmail.com",
+                "Kishore Konate",
+                 null, null
+        );
+        return apiInfo;
     }
     
    
